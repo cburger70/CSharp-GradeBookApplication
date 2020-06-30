0ca10710538c671342ba43bef6799c357e8bce34
cburger70 <67466067+cburger70@users.noreply.github.com>
2020-06-30 15:23:06 -0500

    C# Beginner project 3.3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,19 +16,23 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
 
 
-                int threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            }
+                var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-
-
-            return base.GetLetterGrade(averageGrade);
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
