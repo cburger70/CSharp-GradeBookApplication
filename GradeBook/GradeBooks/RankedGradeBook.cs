ffe6f797a2c065906faf46cc862bbb8b8abd5e3e
cburger70 <67466067+cburger70@users.noreply.github.com>
2020-06-30 10:30:04 -0500

    C# Beginner project 3.1.5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,11 +17,10 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students.);
-    
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
 
 
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+                int threshold = (int)Math.Ceiling(Students.Count * 0.2);
             }
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
