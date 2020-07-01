990bd4a0bc2875ce553e5677289633948e545707
cburger70 <67466067+cburger70@users.noreply.github.com>
2020-07-01 09:40:28 -0500

    C# Beginner 7.3.2

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,6 +6,11 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
+        public StandardGradeBook(string name)
+        {
+            Name = name;
+        }
+
         public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Standard;
